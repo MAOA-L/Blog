0bda6d03163adbfb77c9deb53fa26dc9d025491d
CyanZoy <1099312914@qq.com>
2020-03-06 17:30:18 +0800

    添加asgi.py文件

---
+++ Blog/asgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for Blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blog.settings")
+django.setup()
+application = get_default_application()
